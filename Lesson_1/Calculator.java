213d1fe041fabf225b446a59db07c093a08789ba
den <denlatypoff@yandex.ru>
2023-06-11 14:28:41 +0300

    update Calculator.java

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -1,48 +1,30 @@
 public class Calculator {
     public static void  main(String[] args) {
-
         System.out.println("\nКалькулятор : ");
 
         int num1 = 2;
         int num2 = 4;
-        int count = 1;
-
-        char plus = '+';
-        char minus = '-';
-        char multi = '*';
-        char div = '/';
-        char degree = '^';
-        char mod = '%';
-        char sign = ' ';
-
-        char choicePlayer = '^';
+        char sign = '^';
         int result = 0;
 
-        while (count != 0) {
-
-            if (choicePlayer == plus) {
-                result = num1 + num2;
-                sign = plus;
-            } else if (choicePlayer == minus) {
-                result = num1 - num2;
-                sign = minus;
-            } else if (choicePlayer == multi) {
-                result = num1 * num2;
-                sign = multi;
-            } else if (choicePlayer == div) {
-                result = num1 / num2;
-                sign = div;
-            } else if (choicePlayer == degree) {
-                for (int i = 0; i < num2; i++) {
-                    result = num1 * num2; 
-                }
-                sign = degree;
-            } else if (choicePlayer == mod) {
-                result = num1 % num2;
-                sign = mod;
+        if (sign == '+') {
+            result = num1 + num2;
+        } else if (sign == '-') {
+            result = num1 - num2;
+        } else if (sign == '*') {
+            result = num1 * num2;
+        } else if (sign == '/') {
+            result = num1 / num2;
+        } else if (sign == '^') {
+            int temp = 1;
+            for (int i = 1; i <= num2; i++) {
+                temp = temp * num1; 
             }
-            count -= 1;
+            result = temp;
+        } else if (sign == '%') {
+            result = num1 % num2;
         }
+
         System.out.println(num1 + " " + sign + " " + num2 + " = " + result);
     }
 }
