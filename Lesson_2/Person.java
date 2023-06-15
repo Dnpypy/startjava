5e9d2fd5b38c387db81edf3ce71c05a8b5e53944
den <denlatypoff@yandex.ru>
2023-06-15 12:19:41 +0300

    create folder and Person.java

---
+++ Lesson_2/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+    String gender = "male";
+    String name = "Denis";
+    int height = 175;
+    int weight = 75;
+    int age = 41;
+
+    void walk () {
+
+    }
+
+    void sit () {
+
+    }
+
+    void run () {
+
+    }
+
+    void talk () {
+
+    }
+
+    void learn () {
+
+    }
+}
