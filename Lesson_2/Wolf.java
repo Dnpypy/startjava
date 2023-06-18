ae52978e2210910684dc0f63309e34877ea6dda1
den <denlatypoff@yandex.ru>
2023-06-18 21:56:23 +0300

    add two files Lesson_2

---
+++ Lesson_2/Wolf.java
@@ -0,0 +1,23 @@
+public class Wolf {
+    public String gender;
+    public String name; 
+    public int weight;
+    public int age; 
+    public String color;
+
+    public void walk() {
+        System.out.println("walk");
+    } 
+    public void sit() {
+        System.out.println("sit");
+    } 
+    public void run() {
+        System.out.println("run");
+    } 
+    public void howl() {
+        System.out.println("howls");
+    } 
+    public void hunt() {
+        System.out.println("hunted");
+    }
+}
---
+++ Lesson_2/WolfTest.java
@@ -0,0 +1,20 @@
+public class WolfTest {
+    public static void main(String[] args) {
+        Wolf wolf1 = new Wolf();
+        wolf1.gender = "male";
+        wolf1.name = "barsik";
+        wolf1.weight = 40;
+        wolf1.age = 8;
+        wolf1.color = "ginger";
+
+        System.out.println(wolf1.gender);
+        System.out.println(wolf1.name);
+        System.out.println(wolf1.weight);
+        System.out.println(wolf1.age);
+        System.out.println(wolf1.color);
+
+        
+    }
+    
+
+}
