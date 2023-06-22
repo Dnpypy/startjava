98fe1f078862b074e69847c9ba6b1837e800dd9e
den <denlatypoff@yandex.ru>
2023-06-22 11:08:51 +0300

    update Wolf.java

--- Lesson_2/Wolf.java
+++ Lesson_2/Wolf.java
@@ -8,15 +8,19 @@ public class Wolf {
     public void walk() {
         System.out.println("walk");
     } 
+
     public void sit() {
         System.out.println("sit");
     } 
+
     public void run() {
         System.out.println("run");
     } 
+
     public void howl() {
         System.out.println("howls");
     } 
+    
     public void hunt() {
         System.out.println("hunted");
     }
