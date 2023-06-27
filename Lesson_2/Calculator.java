c908657076a8c5b41459e1f37dbbb9cbf404d837
den <denlatypoff@yandex.ru>
2023-06-27 12:00:15 +0300

    update Calculator.java CalculatorTest.java

--- Lesson_2/Calculator.java
+++ Lesson_2/Calculator.java
@@ -1,85 +1,72 @@
 
 public class Calculator {
 
-    private int cycle; 
-    public int num1;
-    public int num2;
-    public char sign;
-    public static char signPrint; // для отображение знака в выводе и для вычисления
+    private int num1;
+    private int num2;
+    private char sign;
     
-    public Calculator(int cycle) {
-        this.cycle = cycle;
-    }
-
-    public void setCycle(int cycle) {
-        this.cycle = cycle;
-    }
-
-    public int getCycle() {
-        return cycle;
-    }
-
-    public int getNum1(){
+    public int getNum1() {
         return num1;
     }
 
-    public int getNum2(){
+    public void setNum1(int num1) {
+        this.num1 = num1;
+    }
+
+    public int getNum2() {
         return num2;
     }
 
-    public char getSign(){
+    public void setNum2(int num2) {
+        this.num2 = num2;
+    }
+
+    // проверка математической операции
+    public boolean setSign(char sign) {
+        this.sign = sign;
+        if (sign == '+' || sign == '-' || sign == '*' || sign == '/' || sign == '^' || sign ==  '%') {
+            return false;
+        } else {
+            System.out.println("введенная математическая операция не поддерживается!");
+            return true;
+        }
+        
+    }
+
+    public char getSign() {
         return sign;
     }
 
     // вычисления
-    public void calculations(int num1, int num2) {
+    public void calculate() {
         var result = 0;
-        switch (signPrint) {
+        switch (getSign()) {
             case '+' :
-                result = num1 + num2;
-                System.out.println(result);
+                result = getNum1() + getNum2();
+                //System.out.println(result);
                 break;
             case '-' :
-                result = num1 - num2;
+                result = getNum1() - getNum2();
                 break;
             case '*' :
-                result = num1 * num2;
+                result = getNum1() * getNum2();
                 break;
             case '/' :
-                result = num1 / num2;
+                result = getNum1() / getNum2();
                 break;
             case '^' :
                 var temp = 1;
-                for (int i = 1; i <= num2; i++) {
-                    temp *= num1; 
+                for (int i = 1; i <= getNum2(); i++) {
+                    temp *= getNum1(); 
                 }
                 result = temp;
               break;
             case '%' :
-                result = num1 % num2;
+                result = getNum1() % getNum2();
                 break;
             default :
                 break;
             }
-            System.out.println(num1 + " " + signPrint + " " + num2 + " = " + result);
-    }
-
-    // проверка математического знака
-    public int checkSign(char ch) {
-        signPrint = ch;
-        if (ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '^' || ch ==  '%') {
-            return 0;
-        } else {
-            errorTextSign();
-            return 1;
-        }
-    }
-
-    void errorTextSign() {
-        System.out.println("введенная математическая операция не поддерживается!");
-    }
-
-    void textSign() {
-        System.out.println("Введите знак математической операции: ");
+            System.out.println(getNum1() + " " + getSign() + " " + getNum2() + " = " + result);
     }
 }
--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -3,29 +3,29 @@ import java.util.Scanner;
 public class CalculatorTest {
     public static void  main(String[] args) {
         System.out.println("\nКалькулятор : \n");
-        Calculator calc = new Calculator(2);
+        Calculator calc = new Calculator();
         Scanner sc = new Scanner(System.in);
-        
-        while (calc.getCycle() > 1) {
+        int cycle = 2;
+
+        while (cycle > 1) {
             System.out.println("Введите первое число: ");
-            calc.num1 = sc.nextInt();
+            calc.setNum1(sc.nextInt());
             
-            var cycle2 = 1;
-            while (cycle2 == 1) {
-                calc.textSign();
-                cycle2 = calc.checkSign(sc.next().charAt(0));
+            var cycle2 = true;
+            while (cycle2) {
+                System.out.println("Введите знак математической операции: ");
+                cycle2 = calc.setSign(sc.next().charAt(0));
             }
             
             System.out.println("Введите второе число: ");
-            calc.num2 = sc.nextInt();
+            calc.setNum2(sc.nextInt());
             
-            calc.calculations(calc.num1, calc.num2);
+            calc.calculate();
             
             System.out.println("Хотите продолжить вычисления? [yes/no]: ");
             var yesNo = sc.next();
             if (yesNo.equals("no")) {
-                var temp = calc.getCycle();
-                calc.setCycle(temp -= 1);
+                cycle -= 1;
                 sc.close();
             } 
         }
