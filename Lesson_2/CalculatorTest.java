cf096e2fa35f31411cd1a00d43f86f697ab86616
den <denlatypoff@yandex.ru>
2023-07-05 12:35:11 +0300

    update Calculator.java CalculatorTest.java

--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -7,14 +7,23 @@ public class CalculatorTest {
         Scanner scanner = new Scanner(System.in);
         String option = "yes";
 
-        while(true) {
-            System.out.println("Введите первое число: ");
-            calc.setA(scanner.nextInt());
-            System.out.println("Введите знак математической операции: ");
-            calc.setSign(scanner.nextLine().charAt(0));
-            System.out.println("Введите второе число: ");
-            calc.setB(scanner.nextInt());
-            calc.calculate();
+        while(!option.equals("no")) {
+            while (true) {
+                try {
+                    System.out.println("Введите первое число: ");
+                    calc.setA(Integer.parseInt(scanner.nextLine()));
+                    System.out.println("Введите знак математической операции: ");
+                    calc.setSign(scanner.nextLine().charAt(0));
+                    System.out.println("Введите второе число: ");
+                    calc.setB(Integer.parseInt(scanner.nextLine()));
+                    calc.calculate();
+                    break;
+                } catch (NumberFormatException e) {
+                    System.out.println("NumberFormatException");
+                }
+            }
+             
+            
 
             do {
                 System.out.println("Хотите продолжить вычисления? [yes/no]: ");
@@ -22,31 +31,4 @@ public class CalculatorTest {
             } while(!option.equals("yes") && !option.equals("no"));
         }
     }
-
-    // проверка на ответ yes/no
-    // private static boolean isNext(String word) {
-    //     if (word.equals("no")) {
-    //         return true;
-    //     } else if (word.equals("yes")) {
-    //         return false;
-    //     } 
-    //     return false;
-    // }
-
-    // private static int checkNum(Scanner scanner) {
-    //     var cycle = 0;
-    //     var num = 0;
-       
-    //     do {
-    //         try {
-    //             num = Integer.parseInt(scanner.nextLine());
-    //             cycle = 0;
-    //         } catch (NumberFormatException ex) {
-    //             cycle = 1;
-    //         } catch (NullPointerException ex) {
-    //             cycle = 1;
-    //         }
-    //     } while(cycle == 1);
-    //     return num;
-    // }
 }
