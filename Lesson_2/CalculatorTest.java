63d7f6d8fa6f01a210567ba3c0f011b11abdfe87
den <denlatypoff@yandex.ru>
2023-07-05 11:56:30 +0300

    update Calculator.java CalculatorTest.java

--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -5,65 +5,48 @@ public class CalculatorTest {
         System.out.println("\nКалькулятор : \n");
         Calculator calc = new Calculator();
         Scanner scanner = new Scanner(System.in);
+        String option = "yes";
 
         while(true) {
             System.out.println("Введите первое число: ");
-            calc.setNum1(checkNum(scanner));
-            
-            while (true) {
-                System.out.println("Введите знак математической операции: ");
-                var word = scanner.nextLine();
-                if (word.isEmpty()) {
-                    continue;
-                } else {
-                    if (calc.setSign(word.charAt(0))) {
-                        break;
-                    } 
-                }
-                
-            }
+            calc.setA(scanner.nextInt());
+            System.out.println("Введите знак математической операции: ");
+            calc.setSign(scanner.nextLine().charAt(0));
             System.out.println("Введите второе число: ");
-            calc.setNum2(checkNum(scanner));
+            calc.setB(scanner.nextInt());
             calc.calculate();
-            
-            while(true) {
+
+            do {
                 System.out.println("Хотите продолжить вычисления? [yes/no]: ");
-                var word = scanner.nextLine().trim().toLowerCase();
-                if (word.equals("yes") || word.equals("no")) {
-                   if (isNext(word)) {
-                        System.exit(0);
-                    } else if (isNext(word) == false) {
-                        break;
-                    } 
-                }
-            }
+                option = scanner.nextLine();
+            } while(!option.equals("yes") && !option.equals("no"));
         }
     }
 
     // проверка на ответ yes/no
-    private static boolean isNext(String word) {
-        if (word.equals("no")) {
-            return true;
-        } else if (word.equals("yes")) {
-            return false;
-        } 
-        return false;
-    }
+    // private static boolean isNext(String word) {
+    //     if (word.equals("no")) {
+    //         return true;
+    //     } else if (word.equals("yes")) {
+    //         return false;
+    //     } 
+    //     return false;
+    // }
 
-    private static int checkNum(Scanner scanner) {
-        var cycle = 0;
-        var num = 0;
+    // private static int checkNum(Scanner scanner) {
+    //     var cycle = 0;
+    //     var num = 0;
        
-        do {
-            try {
-                num = Integer.parseInt(scanner.nextLine());
-                cycle = 0;
-            } catch (NumberFormatException ex) {
-                cycle = 1;
-            } catch (NullPointerException ex) {
-                cycle = 1;
-            }
-        } while(cycle == 1);
-        return num;
-    }
+    //     do {
+    //         try {
+    //             num = Integer.parseInt(scanner.nextLine());
+    //             cycle = 0;
+    //         } catch (NumberFormatException ex) {
+    //             cycle = 1;
+    //         } catch (NullPointerException ex) {
+    //             cycle = 1;
+    //         }
+    //     } while(cycle == 1);
+    //     return num;
+    // }
 }
