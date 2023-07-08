23b004ea7350b2f895fcc880c4f9d882e010ee66
den <denlatypoff@yandex.ru>
2023-07-08 12:43:33 +0300

    update run test JaegerTest.java

--- Lesson_2/JaegerTest.java
+++ Lesson_2/JaegerTest.java
@@ -10,7 +10,27 @@ public class JaegerTest {
         bracerPhoenix.setG(9);
 
         System.out.println(bracerPhoenix.getA());
+        System.out.println(bracerPhoenix.getB());
+        System.out.println(bracerPhoenix.getC());
+        System.out.println(bracerPhoenix.getD());
+        System.out.println(bracerPhoenix.getE());
+        System.out.println(bracerPhoenix.getF());
+        System.out.println(bracerPhoenix.getG());
+        bracerPhoenix.move();
+        System.out.println(bracerPhoenix.scanKaiju());
+        bracerPhoenix.useVortexCannon();
+        bracerPhoenix.setG(12);
+        System.out.println(bracerPhoenix.getG());
 
         Jaeger strikerEureka = new Jaeger("Striker Eureka", "Mark-5", "USA", 76.2f, 1.85f, 10, 9);
+        System.out.println(strikerEureka.getA());
+        System.out.println(strikerEureka.getB());
+        System.out.println(strikerEureka.getC());
+        System.out.println(strikerEureka.getD());
+        System.out.println(strikerEureka.getE());
+        System.out.println(strikerEureka.getF());
+        System.out.println(strikerEureka.getG());
+        strikerEureka.move();
+        System.out.println(strikerEureka.scanKaiju());
     }
 }
