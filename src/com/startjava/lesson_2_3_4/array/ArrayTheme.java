52691add6d9e7f31ca77f65da65fce91dab0343a
den <denlatypoff@yandex.ru>
2023-08-01 19:08:36 +0300

    update 1, 2, 3 exercise

--- src/com/startjava/lesson_2_3_4/array/ArrayTheme.java
+++ src/com/startjava/lesson_2_3_4/array/ArrayTheme.java
@@ -8,80 +8,71 @@ public class ArrayTheme {
     }
 
     public static void reverseArray() {
-        System.out.println("\n1.Реверс значений массива : ");
+        System.out.println("\n1.Реверс значений массива: ");
         int[] reverseArray = {1, 7, 4, 5, 2, 6, 3};
-        System.out.print("   До реверса : ");
-        for(int i : reverseArray) { 
-            System.out.print(i + " ");
-        }
-        var temp = 0;
-        for(int i = 0; i < reverseArray.length; i++) {
-            temp = reverseArray[i];
-            reverseArray[i] = reverseArray[reverseArray.length - i - 1];
-            reverseArray[reverseArray.length - i - 1] = reverseArray[i];
+        System.out.print("   До реверса: ");
+        printArray(reverseArray);
+        var length = reverseArray.length;
+        for(int i = 0; i < length; i++) {
+            length--;
+            var temp = reverseArray[i];
+            reverseArray[i] = reverseArray[length];
+            reverseArray[length] = temp;
         }
 
         System.out.println();
-        System.out.print("После реверса : ");
-        for(int i : reverseArray) { 
-            System.out.print(i + " ");
-        }
+        System.out.print("После реверса: ");
+        printArray(reverseArray);
     }
 
     public static void multiplyArrayElements() {
-        System.out.println("\n\n2.Произведение элементов массива : ");
-        int[] multiplyArray = new int[9];
-        var sum = 0;
-        for (int i = 0; i < multiplyArray.length; i++) {
-            multiplyArray[i] += i; 
-            sum *= multiplyArray[i]; 
+        System.out.println("\n\n2.Произведение элементов массива: ");
+        int[] multipliers = new int[9];
+        var multi = 1;
+        var length = multipliers.length;
+        for (int i = 1; i < length; i++) {
+            multipliers[i] = i; 
+            multi *= multipliers[i];
         }
-
-        for(int i : multiplyArray) {
-            System.out.print(i == 8 ? i + " = " + sum : i + " * ");
+        for (int i = 1; i < length; i++) {
+            System.out.print(i == (length - 1) ? i + " = " + multi : i + " * ");
         }
     }
 
     public static void deleteArrayElements() {
-        System.out.println("\n\n3.Удаление элементов массива : ");
-        float[] floatArray = new float[15];
-        var sumIndex = 0;
-        for(int i = 0; i < floatArray.length; i++) {
-            sumIndex += i;
-            float tempFl = (float)Math.random();
-            floatArray[i] = tempFl;
+        System.out.println("\n\n3.Удаление элементов массива: ");
+        float[] realNumbers = new float[15];
+        var length = realNumbers.length;
+        for(int i = 0; i < length; i++) {
+            realNumbers[i] = (float) Math.random();
         }
-        System.out.println("исходный массив : ");
-        for(int i = 0; i < floatArray.length; i++) {
-            if (i == 8) { System.out.println(); };
-            System.out.printf("%.3f ",floatArray[i]);
-        }
+
+        System.out.println("исходный массив: ");
+        printFloatArray(realNumbers);
         
-        var averageIndex = sumIndex / floatArray.length;
+        var averageIndex = length / 2;
         var zeroIndex = 0;
-        for(int i = 0; i < floatArray.length; i++) {
-            if (floatArray[averageIndex] < floatArray[i]) {
-                floatArray[i] = 0.0f;
+        for(int i = 0; i < length; i++) {
+            if (realNumbers[averageIndex] < realNumbers[i]) {
+                realNumbers[i] = 0.0f;
                 zeroIndex++;
             }
         }
-        System.out.println("\nИзмененный массив : ");
-        for(int i = 0; i < floatArray.length; i++) {
-            if (i == 8) { System.out.println(); };
-            System.out.printf("%.3f ",floatArray[i]);
-        }
-        System.out.println("\nКоличество обнуленных ячеек : " + zeroIndex);
+        System.out.println("\nИзмененный массив: ");
+        printFloatArray(realNumbers);
+        System.out.println("\nКоличество обнуленных ячеек: " + zeroIndex);
     }
 
     public static void outputAlphabet() {
-        System.out.println("\n4.Вывод алфавита лесенкой : ");
+        System.out.println("\n4.Вывод алфавита лесенкой: ");
         char[] charArray = new char[26];
-        int z = 0; 
+        var z = 0;
+        var length = charArray.length; 
         for(char ch = 'Z'; ch >= 'A'; ch--) {
             charArray[z++] = ch;
         }
         
-        for(int i = 1; i <= charArray.length - 1; i++) {
+        for(int i = 1; i <= length - 1; i++) {
             for(int y = 1; y <= i; y++) {
                 System.out.print(charArray[y]);
             }
@@ -90,7 +81,7 @@ public class ArrayTheme {
     }
 
     public static void addArrayUnicElemenets() {
-        System.out.println("\n5.Заполнение массива уникальными числами : ");
+        System.out.println("\n5.Заполнение массива уникальными числами: ");
         int[] uniqueArray = new int[30];
         var length = uniqueArray.length;
         
@@ -112,8 +103,21 @@ public class ArrayTheme {
             
         }
         bubbleSort(uniqueArray);
-        for(int i = 0; i < uniqueArray.length; i++) {
+        for(int i = 0; i < length; i++) {
             System.out.print(i == 10 || i == 20 ? uniqueArray[i] + "\n": uniqueArray[i] + " ");
+        }
+    }
+
+    private static void printFloatArray(float[] array) {
+        for(int i = 0; i < array.length; i++) {
+            if (i == 8) { System.out.println(); };
+            System.out.printf("%.3f ", array[i]);
+        }
+    }
+
+    private static void printArray(int[] array) {
+        for(int i : array) { 
+            System.out.print(i + " ");
         }
     }
 
@@ -128,10 +132,11 @@ public class ArrayTheme {
 
     private static int[] bubbleSort(int[] uniqueArray) {
         boolean isSorted = false;
-        int num = 0;
+        var num = 0;
+        var length = uniqueArray.length;
         while(!isSorted) {
             isSorted = true;
-            for(int i = 0; i < uniqueArray.length - 1; i++) {
+            for(int i = 0; i < length - 1; i++) {
                 if(uniqueArray[i] > uniqueArray[i + 1]) {
                     isSorted = false;
 
