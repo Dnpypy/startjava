adb1256daa701d06747d4f8b3c264f4a5c295df7
den <denlatypoff@yandex.ru>
2023-08-05 13:09:21 +0300

    1 exercises

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -1,17 +1,31 @@
 package com.startjava.lesson_2_3_4.calculator;
 
+import java.lang.Math;
+
 public class Calculator {
 
-    private int num1;
-    private int num2;
+    private double num1;
+    private double num2;
     private char sign;
     
-    public void setNum1(int num1) {
+    public void setNum1(double num1) {
         this.num1 = num1;
     }
 
-    public void setNum2(int num2) {
+    public void setNum2(double num2) {
         this.num2 = num2;
+    }
+
+    public double getNum1() {
+        return num1;
+    }
+
+    public double getNum2() {
+        return num2;
+    }
+
+    public char getSign() {
+        return sign;
     }
 
     // проверка математической операции
@@ -20,38 +34,63 @@ public class Calculator {
             this.sign = sign;
             return true;
         } 
-        System.out.println("введенная математическая операция не поддерживается!");
+        this.sign = sign; // любой знак записывается и для вывода ошибка нужно
         return false;
     }
 
     // вычисления
-    public void calculate() {
-        var result = 0;
-        switch (sign) {
-            case '+' :
-                result = num1 + num2;
-                break;
-            case '-' :
-                result = num1 - num2;
-                break;
-            case '*' :
-                result = num1 * num2;
-                break;
-            case '/' :
-                result = num1 / num2;
-                break;
-            case '^' :
-                result = 1;
-                for (int i = 1; i <= num2; i++) {
-                    result *= num1; 
-                }
-                break;
-            case '%' :
-                result = num1 % num2;
-                break;
-            default:
-                break;
+    // public void calculate() {
+    //     double result = 0.0;
+    //     switch (sign) {
+    //         case '+' :
+    //             result = num1 + num2;
+    //             System.out.println(num1 + " " + sign + " " + num2 + " = " + result);
+    //             break;
+    //             return result;
+    //         case '-' :
+    //             result = num1 - num2;
+    //             System.out.println(num1 + " " + sign + " " + num2 + " = " + result);
+    //             break;
+    //         case '*' :
+    //             result = num1 * num2;
+    //             System.out.println(num1 + " " + sign + " " + num2 + " = " + result);
+    //             break;
+    //         case '/' :
+    //             result = num1 / num2;
+    //             System.out.println(num1 + " " + sign + " " + num2 + " = " + result);
+    //             break;
+    //         case '^' :
+    //             result = Math.pow(num1, num2);
+    //             System.out.println(num1 + " " + sign + " " + num2 + " = " + result);
+    //             break;
+    //         case '%' :
+    //             result = num1 % num2;
+    //             System.out.println(num1 + " " + sign + " " + num2 + " = " + result);
+    //             break;
+    //         default:
+    //             System.out.println("Ошибка: знак " + sign + " не поддерживается");
+    //             break;
+    //     }
+    // }
+
+    // вычисления
+    public double calculate() {
+        if (sign == '+') {
+            return num1 + num2;
+        } else if (sign == '-') {
+            return num1 - num2;
+        } else if (sign == '*') {
+            return num1 * num2;
+        } else if (sign == '/') {
+            return num1 / num2;
+        } else if (sign == '^') {
+            return Math.pow(num1, num2);
+        } else if (sign == '%') {
+            return num1 % num2;
+        } else {
+            System.out.println("Ошибка: знак " + sign + " не поддерживается");
+            return -1.0;
         }
-        System.out.println(num1 + " " + sign + " " + num2 + " = " + result);
     }
 }
+
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -1,6 +1,7 @@
 package com.startjava.lesson_2_3_4.calculator;
 
 import java.util.*;
+import java.text.DecimalFormat;
 
 public class CalculatorTest {
     public static void  main(String[] args) {
@@ -10,18 +11,42 @@ public class CalculatorTest {
         String option = "yes";
 
         while(!option.equals("no")) {
-            System.out.println("Введите первое число: ");
-            calc.setNum1(Integer.parseInt(scanner.nextLine()));
-            System.out.println("Введите знак математической операции: ");
-            calc.setSign(scanner.nextLine().charAt(0));
-            System.out.println("Введите второе число: ");
-            calc.setNum2(Integer.parseInt(scanner.nextLine()));
-            calc.calculate();
-                
+            String[] expression = new String[3];
+            System.out.println("Введите математическое выражение : 2 ^ 10");
+            expression = scanner.nextLine().split(" ");
+            calc.setNum1(Double.parseDouble(expression[0]));
+            System.out.println("expression[1].charAt(0) : " + expression[1].charAt(0));
+            calc.setSign(expression[1].charAt(0));
+            calc.setNum2(Double.parseDouble(expression[2]));
+            double result = 0.0;
+            result = calc.calculate();
+            if (result == 0.0) { // 1.0 - 1.0 = 0  +
+                System.out.println(calc.getNum1() + " " + calc.getSign() + " " + calc.getNum2() + " = " + (int)result);
+            } else if (!(result == -1.0)) { // "Ошибка: знак " + sign + " не поддерживается" +
+                DecimalFormat format = new DecimalFormat("#.000"); // 3 знака после запятой +
+                System.out.println(calc.getNum1() + " " + calc.getSign() + " " + calc.getNum2() + 
+                    " = " + format.format(result));
+            } 
+            clearArray(expression);
+            calc.setSign(' ');
+            
             do {
                 System.out.println("Хотите продолжить вычисления? [yes/no]: ");
                 option = scanner.nextLine();
+
             } while(!option.equals("yes") && !option.equals("no"));
+        }
+    }
+
+    private static void clearArray(String[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = "";
+        }
+    }
+
+    private static void printArray(String[] array) {
+        for(String i : array) { 
+            System.out.print(i + " ");
         }
     }
 }
