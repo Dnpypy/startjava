24719b31d9cedb96eee580bc97b32b31e34702a3
den <denlatypoff@yandex.ru>
2023-08-12 16:00:19 +0300

    Update Calculator.java CalculatorTest.java

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -1,56 +1,26 @@
 package com.startjava.lesson_2_3_4.calculator;
 
 import java.lang.Math;
-import java.util.Scanner;
 
 public class Calculator {
 
-    private double num1;
-    private double num2;
-    private char sign;
-   
-    public void setNum1(double num1) {
-        this.num1 = num1;
-    }
-
-    public double getNum1() {
-        return num1;
-    }
-
-    public void setNum2(double num2) {
-        this.num2 = num2;
-    }
-
-    public double getNum2() {
-        return num2;
-    }
-
-    // проверка математической операции
-    public void setSign(char sign) {
-        if (sign == '+' || sign == '-' || sign == '*' || sign == '/' || sign == '^' || sign ==  '%') {
-            this.sign = sign;
-        } 
-        this.sign = sign; // любой знак записывается и для вывода ошибок нужен
-    }
-
-    public char getSign() {
-        return sign;
-    }
-
     // вычисления
-    public double calculate(String[] array) {
-        setNum1(Double.parseDouble(array[0]));
-        setSign(array[1].charAt(0));
-        setNum2(Double.parseDouble(array[2]));
-
-        System.out.println("sign : " + sign);
+    public double calculate(String exp) {
+        double num1;
+        double num2;
+        char sign;
+        String[] tempArray = exp.split(" ");
+        num1 = Double.parseDouble(tempArray[0]);
+        sign = tempArray[1].charAt(0);
+        num2 = Double.parseDouble(tempArray[2]);
+        
         switch (sign) {
-            case '+':  return num1 + num2; 
-            case '-':  return num1 - num2; 
-            case '*':  return num1 * num2; 
-            case '/':  return num1 / num2; 
-            case '^':  return Math.pow(num1, num2); 
-            case '%':  return num1 % num2; 
+            case '+': return num1 + num2;
+            case '-': return num1 - num2;
+            case '*': return num1 * num2; 
+            case '/': return num1 / num2; 
+            case '^': return Math.pow(num1, num2); 
+            case '%': return num1 % num2; 
             default: 
                 System.out.println("Ошибка: знак " + sign + " не поддерживается");
                 return Double.MIN_VALUE;
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -13,24 +13,22 @@ public class CalculatorTest {
         do {
             if ("yes".equals(option)) {
                 System.out.println("Введите математическое выражение : 2 ^ 10");
-                String[] tempArray = scanner.nextLine().split(" "); // введенное мат.выражение
-                double result = calc.calculate(tempArray);
-                printResult(result, tempArray);
+                String expression = scanner.nextLine();// введенное мат.выражение
+                double result = calc.calculate(expression);
+                printResult(result, expression);
             }
-            
             System.out.println("Хотите продолжить вычисления? [yes/no]: ");
             option = scanner.nextLine();
         } while(!"no".equals(option));
     }
 
-    public static void printResult(double result, String[] tempArray) {
+    public static void printResult(double result, String expression) {
         if (result != Double.MIN_VALUE) {
             if (result % 1 == 0) {
-                System.out.println(tempArray[0] + " " + tempArray[1] + " " + tempArray[2] + " = " + (int) result);
+                System.out.println(expression + " = " + (int) result);
             } else {
                 DecimalFormat format = new DecimalFormat("0.00"); // 3 знака после запятой +
-                System.out.println(tempArray[0] + " " + tempArray[1] + " " + tempArray[2] + 
-                " = " + format.format(result));
+                System.out.println(expression + " = " + format.format(result));
             }
         } 
     }
